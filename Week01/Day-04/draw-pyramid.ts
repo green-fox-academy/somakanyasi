b1ac0e358e8b46fc6232497b3062d0682eb4eb89
somakanyasi <kanyasi.soma@gmail.com>
2018-08-03 10:58:46 +0200

    Added the pyramid exercise.

---
+++ Week01/Day-04/draw-pyramid.ts
@@ -0,0 +1,25 @@
+'use strict';
+
+let lineCount: number = 10;
+
+// Write a program that draws a
+// pyramid like this:
+//
+//    *
+//   ***
+//  *****
+// *******
+//
+// The pyramid should have as many lines as lineCount is
+
+
+let a: string = ' ';
+let b: string = '*';
+for (let i: number = 0; i < lineCount; i++) {
+    for (let j: number = i; j < (lineCount - 1); j++) {
+        a += ' ';
+    }
+    console.log(a + b);
+    a = ' ';
+    b += '**';
+}
---
+++ Week01/Day-04/draw-triangle.ts
@@ -0,0 +1,20 @@
+'use strict';
+
+let lineCount: number = 4;
+
+// Write a program that draws a triangle like this:
+//
+// *
+// **
+// ***
+// ****
+//
+// The triangle should have as many lines as lineCount is
+
+
+
+for (let i: string = '*'; i.length <= lineCount; i += '*') {
+
+    console.log(i);
+}
+
