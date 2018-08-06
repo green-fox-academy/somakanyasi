8a175ab8cbf9927dfa1de909e59856143addab58
somakanyasi <kanyasi.soma@gmail.com>
2018-08-06 17:12:56 +0200

    Added the length comparison, sum-elements and the swap-elements

---
+++ Week02/Day-01/swap-elements.ts
@@ -0,0 +1,17 @@
+'use strict'
+
+// -  Create a variable named `abc` with the following content: `["Arthur", "Boe", "Chloe"]`
+// -  Swap the first and the third element of `abc`
+
+let abc: string [] = ['Arthur', 'Boe', 'Chloe'];
+
+let temp: string = abc[0];
+abc[0] = abc[2];
+abc[2] = temp;
+
+console.log(abc);
+
+
+// [abc[0], abc[2]] = [abc[2], abc[0]];
+
+//console.log(abc);
