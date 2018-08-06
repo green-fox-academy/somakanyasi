d94fdc6f6b0f0b8c5e95fe45c3eafe1470b8ad10
somakanyasi <kanyasi.soma@gmail.com>
2018-08-06 16:19:18 +0200

    'Added print params exercise. Two solutions"

---
+++ Week02/Day-01/print-params.ts
@@ -0,0 +1,23 @@
+'use strict'
+
+
+// -  Create a function called `printParams`
+//    which logs to the console the input parameters
+//    (can have multiple number of arguments)
+
+
+
+function printParams (...myParameter: any[]) {
+    for ( let i: number = 0; i < myParameter.length; i++) {
+        console.log(myParameter[i]);
+    }
+}
+printParams(23, 'szia', 'lécci múkodj', 41);
+
+//ha nem egymás alá szeretnénk kiírni a lista tartalmát akkor nem kell a for loop
+
+
+function printParams2 (...myParameter: any[]) {
+        console.log(myParameter);
+}
+printParams2(23, 'szia', 'lécci múkodj', 41);
