c2fa23173e96ce0b539b5f1211f1392ced8de3d3
somakanyasi <kanyasi.soma@gmail.com>
2018-08-06 23:26:19 +0200

    Added append-a.ts

---
+++ Week02/Day-01/append-a.ts
@@ -0,0 +1,14 @@
+'use strict'
+
+
+// -  Create an array variable named `animals`
+//    with the following content: `["koal", "pand", "zebr"]`
+// -  Add all elements an `"a"` at the end
+
+
+
+let animals = ['koal' , 'pand' , 'zebr'];
+
+animals.forEach(function(animals) {
+    console.log(animals + 'a');
+});
---
+++ Week02/Day-01/change-element.ts
@@ -0,0 +1,16 @@
+'use strict'
+
+// -  Create an array named `numList` with the following content: `[1, 2, 3, 8, 5, 6]`
+// -  Change the 8 to 4 with the `.map` method 
+// -  Print the fourth element as a test
+
+
+let numList: number [] = [1, 2, 3, 8, 5, 6];
+
+
+let newList = numList.map(function (newList) {
+    return [1, 2, 3, 4, 5, 6];
+})
+
+console.log(newList);
+
