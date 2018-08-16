60b8d35fbb39bc4b0833abbd8e431ef2e380ac16
somakanyasi <kanyasi.soma@gmail.com>
2018-08-16 14:06:49 +0200

    Last string exercise. solution with recursion too

--- Week03/Day-04/string3.ts
+++ Week03/Day-04/string3.ts
@@ -2,9 +2,22 @@
 // adjacent chars are now separated by a '*'.
 
 let myString: string = 'Semmi értelme a rekurzív dolgoknak.';
+let myString2: string = 'banán';
+
 
 function addStarAfterEveryChar(inputString: string, separator: string): any {
     return inputString.split('').join(separator);
 }
 
 console.log(addStarAfterEveryChar(myString, '*'));
+
+
+function addStarsWithRecursion(inputString: string): string {
+    if (inputString.length > 0) {
+        return inputString.slice(0, 1) + '*' + addStarsWithRecursion(inputString.slice(1));
+    } else {
+        return 'The End!';
+    }
+}
+
+console.log(addStarsWithRecursion(myString));
