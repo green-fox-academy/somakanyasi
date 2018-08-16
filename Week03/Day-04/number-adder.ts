eb7ee3bc569fbf01898e723cad83f195b4eab05d
somakanyasi <kanyasi.soma@gmail.com>
2018-08-16 11:09:23 +0200

    number-adder exercise: finished with recursion

---
+++ Week03/Day-04/number-adder.ts
@@ -0,0 +1,13 @@
+// Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
+
+function addingNumbers(n: number): number {
+    if (n < 0) {
+        console.log('Don\'t use negative numbers dummie!');
+    } else if(n == 1) {
+        return 1;
+    } else {
+        return n + addingNumbers(n - 1);
+    }
+}
+
+console.log(addingNumbers(10));
