5303771df6e4cc3d747f3f5b4b9ce74899d04ced
somakanyasi <kanyasi.soma@gmail.com>
2018-09-15 14:49:54 +0200

    add(feat) button tag added to switch pictures

--- Week06/Day-05/galery-project/index.js
+++ Week06/Day-05/galery-project/index.js
@@ -9,6 +9,12 @@ app.set('view engine', 'ejs');
 
 app.use('/assets', express.static('assets'));
 
+const myPictures = [
+  'https://i.cdn.turner.com/adultswim/big/img/2018/05/07/rick-and-morty.jpg',
+  'https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fimage%2F2018%2F06%2Frick-morty-trading-card-set-1.jpg?q=75&w=800&cbr=1&fit=max',
+  'https://3m84r11gpx1j11puas2g5wfl-wpengine.netdna-ssl.com/wp-content/uploads/2017/09/RickMortyTerryFolds.jpg'
+];
+
 app.get('/', (req, res) => {
   res.render('home', {
     message: 'picture here',
