b11149f913d87a0ef6a3ce549a49bda60d6b7b4e
somakanyasi <kanyasi.soma@gmail.com>
2018-09-25 17:10:29 +0200

    add(exercise): candy is almost done. only make it rain function is missing

--- Week08/Day-02/candy/game.js
+++ Week08/Day-02/candy/game.js
@@ -1,2 +1,50 @@
 'use strict';
 
+const candyButton = document.querySelector('.create-candies');
+const lollyButton = document.querySelector('.buy-lollypops');
+const candyMachine = document.querySelector('.candy-machine');
+
+const candies = document.querySelector('.candies');
+const lollypops = document.querySelector('.lollypops');
+const speed = document.querySelector('.speed');
+
+let candyNumber = 10000;
+let lollypopNumber = 3;
+let speedRatio = 0;
+let currentLollies = lollypops.textContent;
+
+candies.textContent = candyNumber;
+
+candyButton.addEventListener('click', () => {
+  candyNumber ++;
+  candies.textContent = candyNumber;
+});
+
+lollyButton.addEventListener('click', () => {
+  if (candyNumber >= 100) {
+    candyNumber -= 100;
+    lollypopNumber ++;
+    candies.textContent = candyNumber;
+    lollypops.textContent = `${currentLollies}🍭`;
+    currentLollies = lollypops.textContent;
+  } else {
+    lollyButton.disabled = true;
+  }
+});
+
+const lollyPopForCandies = () => {
+  if (lollypopNumber >= 10) {
+    candyNumber = candyNumber + speedRatio;
+    candies.textContent = candyNumber;
+    candyCreationRatio();
+  }
+}
+
+const candyCreationRatio = () => {
+  speedRatio = Math.floor(lollypopNumber / 10);
+  speed.textContent = speedRatio;
+}
+
+setInterval(lollyPopForCandies, 1000);
+
+candyMachine.addEventListener('click', )
