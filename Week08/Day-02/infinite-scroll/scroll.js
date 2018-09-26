0509e9dbd8d40cbf099147322c872b577612e205
somakanyasi <kanyasi.soma@gmail.com>
2018-09-26 21:38:39 +0200

    add(function) div creation ready

--- Week08/Day-02/infinite-scroll/scroll.js
+++ Week08/Day-02/infinite-scroll/scroll.js
@@ -9,3 +9,15 @@ const getRandomNum = () => {
 const getRandomColor = () => {
   return `rgb(${getRandomNum()},${getRandomNum()},${getRandomNum()})`;
 };
+
+const randomDivCreator = () => {
+  return Math.floor(Math.random() * 10 + 10);
+}
+
+const bottomDivCreator = () => {
+  for (let i = 0; i < randomDivCreator(); i++) {
+    let div = document.createElement('div');
+    div.style.backgroundColor = getRandomColor();
+    body.appendChild(div);
+  }
+}
