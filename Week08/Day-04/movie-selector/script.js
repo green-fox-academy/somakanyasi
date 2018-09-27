cd59e962a72b000c2813b13de224fd628d42388e
somakanyasi <kanyasi.soma@gmail.com>
2018-09-27 18:07:43 +0200

    selector functionality working

--- Week08/Day-04/movie-selector/script.js
+++ Week08/Day-04/movie-selector/script.js
@@ -5,7 +5,33 @@ window.onload = () => {
   const genres = document.querySelector('#genre');
   const movies = document.querySelector('#movies');
 
-  console.log(genres);
+  genres.addEventListener('change', (event) => {
+    if (event.target.value === "sci-fi") {
+      for (let i = 0; i < movies.length; i++) {
+        if (movies[i].className === "sci-fi") {
+          movies[i].disabled = false;
+        } else {
+          movies[i].disabled = true;
+        }
+      }
+    } else if (event.target.value === "drama") {
+      for (let i = 0; i < movies.length; i++) {
+        if (movies[i].className === "drama") {
+          movies[i].disabled = false;
+        } else {
+          movies[i].disabled = true;
+        }
+      }
+    } else if (event.target.value === "comedy") {
+      for (let i = 0; i < movies.length; i++) {
+        if (movies[i].className === "comedy") {
+          movies[i].disabled = false;
+        } else {
+          movies[i].disabled = true;
+        }
+      }
+    }
+  });
 
 
 }
