3b9c374275cdd9d11a3d9d4d818ea2a33ce5de52
somakanyasi <kanyasi.soma@gmail.com>
2018-09-27 16:05:53 +0200

    cat fact button enabling is working

--- Week08/Day-04/button-disabled/script.js
+++ Week08/Day-04/button-disabled/script.js
@@ -2,10 +2,16 @@
 
 const petButtons = document.querySelector('.petButtons');
 const signupButton  = document.querySelector('.signupButton');
+const catFactButtons = document.querySelector('.catFactButtons');
+const loveCatsButton = document.querySelector('.loveCatsButton');
 
 console.log(petButtons);
 
 
 petButtons.addEventListener('change', () => {
   signupButton.disabled = false;
+});
+
+catFactButtons.addEventListener('change', () => {
+  loveCatsButton.disabled = false;
 });
