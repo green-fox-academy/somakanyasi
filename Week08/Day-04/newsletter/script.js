6ad22b26cbb1b306da50661de3cda856250caedf
somakanyasi <kanyasi.soma@gmail.com>
2018-09-27 20:49:45 +0200

    server gets data and sends back the required response

---
+++ Week08/Day-04/newsletter/script.js
@@ -0,0 +1,8 @@
+// 'use strict';
+
+// window.onload = () => {
+//   const form = document.querySelector('form');
+//   form.addEventListener('submit', (event) => {
+//     event.preventDefault();
+//   });
+// }
--- Week08/Day-04/newsletter/server.js
+++ Week08/Day-04/newsletter/server.js
@@ -5,9 +5,18 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
+
+app.use(cors());
+
 const PORT = 3000;
+
+app.post('/signup', (req, res) => {
+  console.log(req.body);
+  res.json(`Thanks ${req.body.username} we will send updates to ${req.body.email}`);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
 });
-
