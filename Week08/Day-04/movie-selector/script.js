f7ce9dbc2cf6c66e9dd206c29eb5c9d7816f1e33
somakanyasi <kanyasi.soma@gmail.com>
2018-09-27 17:33:12 +0200

    classes added to movies

--- Week08/Day-04/movie-selector/script.js
+++ Week08/Day-04/movie-selector/script.js
@@ -1,8 +1,11 @@
 'use strict';
 
 window.onload = () => {
-  
 
+  const genres = document.querySelector('#genre');
+  const movies = document.querySelector('#movies');
+
+  console.log(genres);
 
 
 }
