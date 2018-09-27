0f0788168bb7c2da4965c623a7079ce195d9b34c
somakanyasi <kanyasi.soma@gmail.com>
2018-09-27 18:18:01 +0200

    select functionality refactored

--- Week08/Day-04/movie-selector/script.js
+++ Week08/Day-04/movie-selector/script.js
@@ -5,30 +5,40 @@ window.onload = () => {
   const genres = document.querySelector('#genre');
   const movies = document.querySelector('#movies');
 
+  // genres.addEventListener('change', (event) => {
+  //   if (event.target.value === "sci-fi") {
+  //     for (let i = 0; i < movies.length; i++) {
+  //       if (movies[i].className === "sci-fi") {
+  //         movies[i].disabled = false;
+  //       } else {
+  //         movies[i].disabled = true;
+  //       }
+  //     }
+  //   } else if (event.target.value === "drama") {
+  //     for (let i = 0; i < movies.length; i++) {
+  //       if (movies[i].className === "drama") {
+  //         movies[i].disabled = false;
+  //       } else {
+  //         movies[i].disabled = true;
+  //       }
+  //     }
+  //   } else if (event.target.value === "comedy") {
+  //     for (let i = 0; i < movies.length; i++) {
+  //       if (movies[i].className === "comedy") {
+  //         movies[i].disabled = false;
+  //       } else {
+  //         movies[i].disabled = true;
+  //       }
+  //     }
+  //   }
+  // });
+
   genres.addEventListener('change', (event) => {
-    if (event.target.value === "sci-fi") {
-      for (let i = 0; i < movies.length; i++) {
-        if (movies[i].className === "sci-fi") {
-          movies[i].disabled = false;
-        } else {
-          movies[i].disabled = true;
-        }
-      }
-    } else if (event.target.value === "drama") {
-      for (let i = 0; i < movies.length; i++) {
-        if (movies[i].className === "drama") {
-          movies[i].disabled = false;
-        } else {
-          movies[i].disabled = true;
-        }
-      }
-    } else if (event.target.value === "comedy") {
-      for (let i = 0; i < movies.length; i++) {
-        if (movies[i].className === "comedy") {
-          movies[i].disabled = false;
-        } else {
-          movies[i].disabled = true;
-        }
+    for (let i = 0; i < movies.length; i++) {
+      if (movies[i].className === event.target.value) {
+        movies[i].disabled = false;
+      } else {
+        movies[i].disabled = true;
       }
     }
   });
