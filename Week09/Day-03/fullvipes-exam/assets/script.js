f463dc2330af340c51b3f8a6947e1c095206405e
somakanyasi <kanyasi.soma@gmail.com>
2018-10-03 16:24:43 +0200

    unused lines deleted

--- Week09/Day-03/fullvipes-exam/assets/script.js
+++ Week09/Day-03/fullvipes-exam/assets/script.js
@@ -1,19 +1,12 @@
 'use strict';
 
 window.onload = () => {
-  const postRequest = new XMLHttpRequest;
   const host = 'http://localhost:7000';
 
   const submitButton = document.querySelector('button');
   const result = document.querySelector('h3');
   const inputMatrix = document.querySelector('#matrixChecker');
 
-  // submitButton.addEventListener('click', (event) => {
-  //   event.preventDefault();
-  //   postRequest.open('POST', `${host}/matrix`, true);
-  //   postRequest.onload = () => {}
-  //   postRequest.send(JSON.stringify(matrix));
-  // })
   submitButton.addEventListener('click', (event) => {
     event.preventDefault();
     fetch(`${host}/matrix`, {
