ac9dce21b60209508280571b92c966980c19b133
somakanyasi <kanyasi.soma@gmail.com>
2018-10-03 22:42:23 +0200

    /game endpoint ready

--- Week09/Day-04/quiz-app/server.js
+++ Week09/Day-04/quiz-app/server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 const conn = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'root',
+  password: '10!GFA25bbTp',
   database: 'quizapp',
 });
 
@@ -32,10 +32,41 @@ app.get('/', (req, res) => {
 });
 
 const randomNumGen = () => {
-  return Math.floor(Math.random() * 10);
+  return Math.floor(Math.random() * 10) + 1;
 }
 
-app.get('/game', (req, res) => {});
+app.get('/game', (req, res) => {
+  let postId = randomNumGen();
+
+  conn.query(`SELECT * 
+  FROM questions 
+  INNER JOIN answers ON questions.id = answers.question_id
+  WHERE questions.id = ${postId}`, (err, result) => {
+    if (err) {
+      console.log('Error connecting to database', err.message);
+      res.status(500).send('Database error');
+      return;
+    }
+    let question = '';
+    let answers = [];
+    let answerData = {}
+    result.forEach(element => {
+      question = element.question;
+      answerData = {
+        "question_id": element.question_id,
+        "id": element.id,
+        "answer_1": element.answer,
+        'is_correct': element.is_correct
+      }
+      answers.push(answerData);
+    })
+    res.status(200).json({
+      id: postId,
+      question: question,
+      answers: answers,
+  })
+  })
+})
 
 app.listen(PORT, () => {
   console.log(`Server is up and running on port ${PORT}`)
