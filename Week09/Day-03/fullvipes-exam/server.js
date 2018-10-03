015f858ae58b24ac73fff8957857eef41d7308f2
somakanyasi <kanyasi.soma@gmail.com>
2018-10-03 11:12:01 +0200

    database created

--- Week09/Day-03/fullvipes-exam/server.js
+++ Week09/Day-03/fullvipes-exam/server.js
@@ -12,6 +12,21 @@ app.use('/assets', express.static('assets'));
 app.use(bodyParser.urlencoded( {extended: false} ));
 app.use(bodyParser.json());
 
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '10!GFA25bbTp',
+  database: 'matrixes',
+});
+
+conn.connect((err) => {
+  if (err) {
+    console.log('Error connecting to Db', err.message);
+    return;
+  }
+  console.log('Connection established');
+});
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
