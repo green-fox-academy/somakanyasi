2b6d9bbe17424bf29ef60c57d0c75d6385546d44
somakanyasi <kanyasi.soma@gmail.com>
2018-10-04 14:38:02 +0200

    score counter and reset button works

--- Week09/Day-04/quiz-app/assets/script.js
+++ Week09/Day-04/quiz-app/assets/script.js
@@ -7,8 +7,16 @@ window.onload = () => {
   const secondAnswer = document.querySelector('#secondAns-button');
   const thirdAnswer = document.querySelector('#thirdAns-button');
   const fourthAnswer = document.querySelector('#fourthAns-button');
+  const scoreNumber = document.querySelector('#score-number');
+  const resetButton = document.querySelector('#reset-button');
 
   const host = 'http://localhost:8080';
+
+  const getCurrentScore = () => {
+    return scoreNumber.textContent
+  }
+
+  let score = localStorage.getItem('score');
 
   fetch(`${host}/game`, {
     method: 'GET',
@@ -17,6 +25,7 @@ window.onload = () => {
     },
   }).then(resp => resp.json())
     .then(response => {
+      scoreNumber.textContent = localStorage.getItem('score');
       displayedQuestion.textContent = response.question;
       firstAnswer.textContent = response.answers[0].answer_1;
       firstAnswer.setAttribute('name', response.answers[0].is_correct);
@@ -27,10 +36,13 @@ window.onload = () => {
       fourthAnswer.textContent = response.answers[3].answer_1;
       fourthAnswer.setAttribute('name', response.answers[3].is_correct);
       quizButton.forEach(element => {
-        console.log(element);
         if (element.name === '1') {
           element.addEventListener('click', (e) => {
             e.preventDefault();
+            score = parseInt(localStorage.getItem('score'));
+            score ++;
+            scoreNumber.textContent = score;
+            localStorage.setItem('score', score);
             element.style.background = 'green';
             setTimeout(() => {
               window.location.reload();
@@ -39,12 +51,16 @@ window.onload = () => {
         } else {
           element.addEventListener('click', (e) => {
             e.preventDefault();
+            scoreNumber.textContent = score;
             element.style.background = 'red';
             setTimeout(() => {
               window.location.reload();
             }, 3000);
           });
         }
+      });
+      resetButton.addEventListener('click', () => {
+        localStorage.setItem('score', 0);
       });
     });
 
