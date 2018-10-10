11ae884bec2d18cabe24f2ac564231d8fd74a3fb
somakanyasi <kanyasi.soma@gmail.com>
2018-10-10 15:08:03 +0200

    objects and classes

--- Week10/first.py
+++ Week10/first.py
@@ -241,5 +241,56 @@ print(text_in_file)
 # os.remove("test.txt")    -   this is how you remove the file
 
 
+#Objects
+
+class Animal:
+  __name = None     #None signifies the lack of a value
+  __height = 0      # the double _ means that these are private
+  __weight = 0
+  __sound = 0
+
+  def __init__(self, name, height, weight, sound):
+    self.__name = name
+    self.__height = height
+    self.__weight = weight
+    self.__sound = sound
+
+  def set_name(self, name):
+    self.__name = name
+
+  def get_name(self):
+    return self.__name
+
+  def set_height(self, height):
+    self.__height = height
+
+  def get_height(self):
+    return self.__height
+
+  def set_weight(self, weight):
+    self.__weight = weight
+
+  def get_weight(self):
+    return self.__weight
+
+  def set_sound(self, sound):
+    self.__sound = sound
+
+  def get_sound(self):
+    return self.__sound
+
+  def get_type(self):
+    print("Animal")
+
+  def toString(self):
+    return "{} is {} cm tall and {} kilograms and say {}".format(self.__name, self.__height, self.__weight, self.__sound)
+
+
+cat = Animal('Whiskers', 33, 10, 'Meow')
+
+print(cat.toString())
+
+
+#Inheritance
 
 
