0dca989e2b35e25165ba4c9aee62966d816d6068
somakanyasi <kanyasi.soma@gmail.com>
2018-10-11 11:38:05 +0200

    classmethods added to professor class

--- Week10/second.py
+++ Week10/second.py
@@ -17,9 +17,6 @@ class CastleKilmereMember:
     return CastleKilmereMember('Redmond Dalodore', 1939, 'male')
 
 
-bromley = CastleKilmereMember('Bromley Huckabee', '1959', 'male')
-print(bromley.says("Jello!"))
-
 class Pupil(CastleKilmereMember):
 
   def __init__(self, name, birthyear, sex, house, start_year, pet = None):
@@ -75,22 +72,21 @@ class Pupil(CastleKilmereMember):
     return f"My name is {self._name} and I'm a student of the {self.house} class {self.start_year}. Also I have an {self.pet_type} and it's name is {self.pet_name}."
 
 
-cleon  = Pupil(name='Cleon Berry',
-               birthyear=2008,
-               sex='Male',
-               house='House of Courage',
-               start_year=2018,
-               pet=('Cotton', 'owl'))
-
-print(cleon.toString())
-print(bromley.location)
 
 class Professor(CastleKilmereMember):
 
-  def __inti__(self, name, birthyear, sex, subject, house=None):
+  def __init__(self, name, birthyear, sex, subject, house):
     super().__init__(name, birthyear, sex)
     self.subject = subject
     self.house = house
+
+  @classmethod
+  def mirren(cls):
+    return cls('Miranda Mirren', 1965, 'female', 'Transfiguration', 'House of Courage')
+
+  @classmethod
+  def blade(cls):
+    return cls('Blade Bardock', 1988, 'male', 'Potions', 'House of Ambition')
 
   
 class Ghost(CastleKilmereMember):
@@ -99,3 +95,19 @@ class Ghost(CastleKilmereMember):
     super().__init__(name, birthyear, sex)
     self.year_of_death = year_of_death
 
+
+    if house is not None:
+      self.house = house
+
+
+if __name__ == "__main__":
+
+  bromley = CastleKilmereMember(name='Bromley Huckabee', birthyear=1959, sex='male')
+  cleon = Pupil(name='Cleon Bery', birthyear=2008, sex='male', house='House of Courage', start_year=2018)
+  headmaster = cleon.school_headmaster()
+
+  mirren = Professor.mirren()
+  blade = Professor.blade()
+  cleon = Pupil.cleon()
+  flynn = Pupil.flynn()
+  cassidy = Pupil.cassidy()
