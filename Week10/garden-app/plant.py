7db7ea8bb9c6a0aefd112692510a6aa23f8e7139
somakanyasi <kanyasi.soma@gmail.com>
2018-10-11 15:46:56 +0200

    started the garden app, plant class created

---
+++ Week10/garden-app/plant.py
@@ -0,0 +1,9 @@
+class Plant:
+    
+    def __init__(self, color, plant_type, water_level, need_water, water_absorb):
+        self.color = color
+        self.plant_type = plant_type
+        self.water_level = water_level
+        self.need_water = need_water
+        self.water_absorb = water_absorb
+        
