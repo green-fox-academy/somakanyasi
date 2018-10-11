3d3bffd100b6770468d20a5641336581e15d7641
somakanyasi <kanyasi.soma@gmail.com>
2018-10-11 15:31:11 +0200

    filter function tested

--- Week10/random_practice.py
+++ Week10/random_practice.py
@@ -13,15 +13,37 @@ print('\n')
 
 #looping over an anumerate object
 for item in enumerate(grocery_list):
-  print(item)
+    print(item)
 
 print('\n')
 
 for count, item in enumerate(grocery_list):
-  print(count, item)
+    print(count, item)
 
 print('\n')
 
 #changing default start value
 for count, item in enumerate(grocery_list, 100):
-  print(count, item)
+    print(count, item)
+
+
+#FILTER
+
+alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'j', 'k', 'm', 'o', 'r', 't', 'v']
+
+#function that filters vowels
+def filterVowels(alphabet):
+    vowels = ['a', 'e', 'i', 'o', 'u']
+
+    if(alphabet in vowels):
+        return True
+    else:
+        return False
+
+filteredVowels = filter(filterVowels, alphabets)
+
+print("The filtered vowels are:")
+for letter in filteredVowels:
+    print(letter)
+
+
