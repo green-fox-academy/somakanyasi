0aa4e23938913e07ac720fbc2b8aecac78c6f394
somakanyasi <kanyasi.soma@gmail.com>
2018-10-11 16:25:20 +0200

    tree class added

--- Week10/garden-app/tree.py
+++ Week10/garden-app/tree.py
@@ -1,15 +1,15 @@
 from plant import Plant
 
-class Flower(Plant):
+class Tree(Plant):
 
     def __init__(self, color):
         super().__init__(color)
-        self.water_absorb = 0.75
+        self.water_absorb = 0.4
 
-        if self.water_level < 5:
+        if self.water_level < 10:
             self.need_water = True
         else:
             self.need_water = False
 
-flower1 = Flower('yellow')
-print(flower1.water_level)
+tree1 = Tree('orange')
+print(tree1.water_level)
