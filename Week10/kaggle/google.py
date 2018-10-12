409cb3f31909fb67b4d122474e2f10df933060e9
somakanyasi <kanyasi.soma@gmail.com>
2018-10-12 13:16:13 +0200

    header messages working

--- Week10/kaggle/google.py
+++ Week10/kaggle/google.py
@@ -2,8 +2,11 @@ import numpy as np
 import pandas as pd
 
 #Read file into dataframe
-# def header (msg):
-# header("Google data file")
+def header (msg):
+    print(msg)
+    print('-' * 50)
+
+header("Google data file")
 
 filename = 'googleplaystore.csv'
 filename2 = 'googleplaystore_user_reviews.csv'
@@ -12,11 +15,11 @@ df2 = pd.read_csv(filename2)
 #print(df)
 
 #all the columns of df
+header('All the columns of the data-frame')
 #print(df.columns)
 
 #sort the records by the rating
+header('Data-frame sorted by the Ratings')
 print(df.sort_values('Rating', ascending=False))
 
-#statistical summary of df
-#print(df.describe())
 
