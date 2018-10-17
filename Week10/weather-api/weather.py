b8bb5aedcebb3817f7c61f3dd7a69fe8a71dd500
somakanyasi <kanyasi.soma@gmail.com>
2018-10-17 10:21:33 +0200

    selected city's data is rendered to html

--- Week10/weather-api/weather.py
+++ Week10/weather-api/weather.py
@@ -8,12 +8,25 @@ CORS(app)
 
 weather_req = requests.get('https://api.openweathermap.org/data/2.5/weather?q=Budapest,hu&units=metric&appid=25dfbbf7d7f7ddb9b7494961dfd68d99')
 
-print(weather_req.json()['main'])
+print(weather_req.json())
 
-@app.route("/", methods=["GET"])
-def template_test():
-    return render_template('weather.html', adat=weather_req.json()['main'])
+@app.route("/", methods=["POST"])
+def home_page():
+    return render_template("index.html")
 
+@app.route("/select", methods=["GET"])
+def selection():
+    return render_template("weather.html", city=(weather_req.json()["name"]), adat=(weather_req.json()["main"]), current_temp=(weather_req.json()["main"]["temp"]), description=(weather_req.json()["weather"][0]["description"]), temp_min=(weather_req.json()["main"]["temp_min"]), temp_max=(weather_req.json()["main"]["temp_max"]))
+
+
+@app.route("/weather", methods=["GET"])
+def return_with_weather():
+    response = app.response_class(
+        response=json.dumps(requests.get('https://api.openweathermap.org/data/2.5/weather?q=Budapest,hu&units=metric&appid=25dfbbf7d7f7ddb9b7494961dfd68d99').text),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
