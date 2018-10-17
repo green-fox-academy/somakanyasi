4d57ef2ef3474023a1300427a3f8f9b1abd6faf4
somakanyasi <kanyasi.soma@gmail.com>
2018-10-17 13:41:30 +0200

    cgi imported in but still not working properly

--- Week10/weather-api/weather.py
+++ Week10/weather-api/weather.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, jsonify, json, request
 from flask_cors import CORS
 import requests
 import lxml
+import cgi
 
 app = Flask(__name__)
 CORS(app)
@@ -14,19 +15,32 @@ print(weather_req.json())
 def home_page():
     return render_template("index.html")
 
-@app.route("/select", methods=["GET"])
-def selection():
-    return render_template("weather.html", city=(weather_req.json()["name"]), adat=(weather_req.json()["main"]), current_temp=(weather_req.json()["main"]["temp"]), description=(weather_req.json()["weather"][0]["description"]), temp_min=(weather_req.json()["main"]["temp_min"]), temp_max=(weather_req.json()["main"]["temp_max"]))
+# @app.route("/select", methods=["GET"])
+# def selection():
+#     return render_template("weather.html", city=(weather_req.json()["name"]), adat=(weather_req.json()["main"]), current_temp=(weather_req.json()["main"]["temp"]), description=(weather_req.json()["weather"][0]["description"]), temp_min=(weather_req.json()["main"]["temp_min"]), temp_max=(weather_req.json()["main"]["temp_max"]))
 
 
-@app.route("/weather", methods=["GET"])
-def return_with_weather():
-    response = app.response_class(
-        response=json.dumps(requests.get('https://api.openweathermap.org/data/2.5/weather?q=Budapest,hu&units=metric&appid=25dfbbf7d7f7ddb9b7494961dfd68d99').text),
-        status=200,
-        mimetype='application/json'
-    )
-    return response
+# @app.route("/weather", methods=["GET"])
+# def return_with_weather():
+#     response = app.response_class(
+#         response=json.dumps(requests.get('https://api.openweathermap.org/data/2.5/weather?q=Budapest,hu&units=metric&appid=25dfbbf7d7f7ddb9b7494961dfd68d99').text),
+#         status=200,
+#         mimetype='application/json'
+#     )
+#     return response
+
+@app.route('/city', methods=["GET"])
+def city_display():
+    print("Second")
+    form = cgi.FieldStorage()
+    print(form.getvalue('chosen_city'))
+    # r = request.get('https://api.openweathermap.org/data/2.5/weather?q='+requested_city+',hu&units=metric&appid=25dfbbf7d7f7ddb9b7494961dfd68d99')
+    # print(r)
+    # json_object = r.json()
+    # temp_k = json_object["main"]["temp"]
+    # print(temp_k)
+    # return render_template("weather.html", current_temp=temp_k)
+    return render_template("weather.html", city="Megkaptam")
 
 if __name__ == '__main__':
     app.run(debug=True)
