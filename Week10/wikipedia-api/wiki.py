08e380d4e250010ce2d1613de8a3ab12d2de989d
somakanyasi <kanyasi.soma@gmail.com>
2018-10-18 11:32:44 +0200

    form added to html

--- Week10/wikipedia-api/wiki.py
+++ Week10/wikipedia-api/wiki.py
@@ -3,6 +3,8 @@ import requests
 
 app = Flask(__name__)
 
+url_start = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search='
+
 @app.route("/", methods=["GET"])
 def home_page():
     return render_template("index.html")
