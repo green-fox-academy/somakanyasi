02499f4ea5c784ee4ddd0badc1418b717cb2d6f2
somakanyasi <kanyasi.soma@gmail.com>
2018-10-19 11:37:37 +0200

    tray except is added to StdOutListener class

--- Week10/twitter-api/twitter.py
+++ Week10/twitter-api/twitter.py
@@ -8,7 +8,6 @@ class TwitterStreamer():
 	'''
 	Class for streaming and processing live tweets
 	'''
-
 	def stream_tweets(self, fetched_tweets_filename, hash_tag_list):
 		#this handles Twitter authentication and the connection to the Twitter Streaming API.
 		listener = StdOutListener()
@@ -24,9 +23,17 @@ class StdOutListener(StreamListener):
 	'''
 	This is a basic listener class that just prints received tweets to stdout
 	'''
+	def __init__(self, fetched_tweets_filename):
+		self.fetched_tweets_filename = fetched_tweets_filename
 
 	def on_data(self, data):
-		print(data)
+		try:
+			print(data)
+			with open(self.fetched_tweets_filename, 'a') as tf:
+				tf.write(data)
+			return True
+		except BaseException as e:
+			print("Error on data: %s" % str(e))
 		return True
 
 	def on_error(self, status):
