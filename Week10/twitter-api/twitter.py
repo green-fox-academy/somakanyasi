94917ddd141800b7b5f274a234abfe8e65e38ec7
somakanyasi <kanyasi.soma@gmail.com>
2018-10-19 11:32:11 +0200

    authentication is moved into a separate class

--- Week10/twitter-api/twitter.py
+++ Week10/twitter-api/twitter.py
@@ -4,7 +4,26 @@ from tweepy import Stream
 
 import twitter_credentials
 
+class TwitterStreamer():
+	'''
+	Class for streaming and processing live tweets
+	'''
+
+	def stream_tweets(self, fetched_tweets_filename, hash_tag_list):
+		#this handles Twitter authentication and the connection to the Twitter Streaming API.
+		listener = StdOutListener()
+		auth = OAuthHandler(twitter_credentials.Twitter_API_key, twitter_credentials.Twitter_secret_API_key)
+		auth.set_access_token(twitter_credentials.Access_Token, twitter_credentials.Access_Token_secret)
+
+		stream = Stream(auth, listener)
+
+		stream.filter(track=hash_tag_list)
+
+
 class StdOutListener(StreamListener):
+	'''
+	This is a basic listener class that just prints received tweets to stdout
+	'''
 
 	def on_data(self, data):
 		print(data)
@@ -15,11 +34,4 @@ class StdOutListener(StreamListener):
 		
 
 if __name__ == "__main__":
-
-	listener = StdOutListener()
-	auth = OAuthHandler(twitter_credentials.Twitter_API_key, twitter_credentials.Twitter_secret_API_key)
-	auth.set_access_token(twitter_credentials.Access_Token, twitter_credentials.Access_Token_secret)
-
-	stream = Stream(auth, listener)
-
-	stream.filter(track=['donald trump'])
+	pass
