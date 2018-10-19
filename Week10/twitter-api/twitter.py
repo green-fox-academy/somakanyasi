5bfd93c4fa2381a11f67c1fe4067c92758836567
somakanyasi <kanyasi.soma@gmail.com>
2018-10-19 13:58:50 +0200

    5 more columns(text length, data, source, likes, retweets) are added to the data frame

--- Week10/twitter-api/twitter.py
+++ Week10/twitter-api/twitter.py
@@ -94,8 +94,13 @@ class TweetAnalyzer():
 	def tweets_to_data_frame(self, tweets):
 		df = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['Tweets'])
 
-		#creates the id column to our dataframe and then fills it out with the proper values
+		#creates the ['.....'] column to our dataframe and then fills it out with the proper values
 		df['id'] = np.array([tweet.id for tweet in tweets])
+		df['length'] = np.array([len(tweet.text) for tweet in tweets])
+		df['date'] = np.array([tweet.created_at for tweet in tweets])
+		df['source'] = np.array([tweet.source for tweet in tweets])
+		df['likes'] = np.array([tweet.favorite_count for tweet in tweets])
+		df['retweets'] = np.array([tweet.retweet_count for tweet in tweets])
 
 		return df
 
