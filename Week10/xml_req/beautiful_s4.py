1ff9189db0572cb536b42671e3fbf326ed5a24cc
somakanyasi <kanyasi.soma@gmail.com>
2018-10-23 18:26:07 +0200

    coffee place names are displayed in the terminal

--- Week10/xml_req/beautiful_s4.py
+++ Week10/xml_req/beautiful_s4.py
@@ -5,10 +5,15 @@ from bs4 import BeautifulSoup
 url = "https://www.yellowpages.com/search?search_terms=coffee&geo_location_terms=Los+Angeles%2C+CA"
 r = requests.get(url)
 
-soup = BeautifulSoup(r.content, 'xml')
+soup = BeautifulSoup(r.content, 'lxml')
 
 links = soup.find_all("a")
 
-for link in links:
-    print("<a href='%s'>%s</a>" %(link.get("href"), link.text))
+# for link in links:
+#     print("<a href='%s'>%s</a>" %(link.get("href"), link.text))
 
+general_data = soup.find_all("div", {"class": "info"})
+
+for item in general_data:
+    print(item.contents[0].find_all("a", {"class": "business-name"})[0].text)
+    print('-------------------------------------------------------')
