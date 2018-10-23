00e7dab14b19b94cb4d2d76050fa870c9f858f5c
somakanyasi <kanyasi.soma@gmail.com>
2018-10-23 17:40:24 +0200

    titles are displayed on the html with the links but too many times

--- Week10/wikipedia-api/wiki.py
+++ Week10/wikipedia-api/wiki.py
@@ -9,7 +9,20 @@ url_start = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&se
 def home_page():
     return render_template("index.html")
 
-
+@app.route("/", methods=["POST"])
+def searching():
+    searched_expression = request.form["search_bar"]
+    url = url_start + searched_expression
+    r = requests.get(url)
+    response = r.json()
+    titles = response[1]
+    links = response[3]
+    link_list = []
+    for index in range(len(titles)):
+        for index in range(len(links)):
+            link_list.append({"title": titles[index], "link": links[index]})
+    print(link_list)
+    return render_template("index.html", response=response, link_list=link_list)
 
 if __name__ == "__main__":
     app.run(debug=True)
