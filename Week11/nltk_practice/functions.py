afed425be6c8aaeba269c9c3efe51eec6c4fba34
somakanyasi <kanyasi.soma@gmail.com>
2018-10-30 15:43:36 +0100

    word comparison in different languages

--- Week11/nltk_practice/functions.py
+++ Week11/nltk_practice/functions.py
@@ -98,7 +98,13 @@ es2en = swadesh.entries(['es', 'en'])    # Spanish-English
 translate = dict(fr2en)
 translate.update(dict(de2en))
 translate.update(dict(es2en))
-print(translate['chien'])
-print(translate['jeter'])
-print(translate['Hund'])
-print(translate['perro'])
+# print(translate['chien'])
+# print(translate['jeter'])
+# print(translate['Hund'])
+# print(translate['perro'])
+
+languages = ['en', 'de', 'nl', 'es', 'fr', 'pt', 'la']
+for i in [139, 140, 141, 142]:
+    print(swadesh.entries(languages)[i])
+
+
