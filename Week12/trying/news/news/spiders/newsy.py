8111a582469b5b0e493f2c23fdc4274b6eb74eda
somakanyasi <kanyasi.soma@gmail.com>
2018-11-06 15:19:47 +0100

    spider is going on all of the pages, gets the title and text

--- Week12/trying/news/news/spiders/newsy.py
+++ Week12/trying/news/news/spiders/newsy.py
@@ -5,6 +5,7 @@ class ClockChangeSpiderTest(scrapy.Spider):
 
     start_urls = [
         'https://24.hu/?s=%C3%B3ra%C3%A1t%C3%A1ll%C3%ADt%C3%A1s',
+        # 'https://24.hu/page/2/?s=%C3%B3ra%C3%A1t%C3%A1ll%C3%ADt%C3%A1s',
     ]
 
     def parse(self, response):
@@ -12,13 +13,26 @@ class ClockChangeSpiderTest(scrapy.Spider):
             next_page = item.css('a::attr(href)').extract_first()
             yield response.follow(next_page, self.parse_article)
 
-    def parse_article(self, response):
-        for title in response.css('')
-            # yield {
-            #     "title": item.css('a::text').extract_first(),
-            #     "link": item.css('a::attr(href)').extract_first(),
-            # }
+        for href in response.css('a.next::attr(href)'):
+            print('linkecskék:', href)
+            yield response.follow(href, self.parse)
+        # next_page = response.css
+        # //*[@id="content"]/div/div/div[2]/div[25]/ul/li[8]/a
 
-        
+    def parse_article(self, response):
+        for article in response.xpath('//div[@class="is_content is_post_false"]'):
+            # print('++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+            print('title:', article.css('h1.post-title span::text').extract_first())
+            print('++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+            # print(article.css('div.post-body p::text').extract())
+            # print('++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+            # print(article.css('span.fb-like-count span::text').extract_first())
+            yield {
+                "title": article.css('h1.post-title span::text').extract_first(),
+                "text": article.css('div.post-body p::text').extract(),
+                "url": article.css('')
+            }
+            
+           
 
 
