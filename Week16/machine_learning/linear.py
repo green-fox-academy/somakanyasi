a0c2fdda933303f6ee69a2ef1d5b85b64e8fb80d
somakanyasi <kanyasi.soma@gmail.com>
2018-11-29 00:47:57 +0100

    b calculated

--- Week16/machine_learning/linear.py
+++ Week16/machine_learning/linear.py
@@ -5,10 +5,13 @@ import matplotlib.pyplot as plt
 xs = np.array([1,2,3,4,5,6], dtype=np.float64)
 ys = np.array([5,4,6,5,6,7], dtype=np.float64)
 
-def best_fit_slope(xs, ys):
+def best_fit_slope_and_intercept(xs, ys):
     m = (((mean(xs) * mean(ys)) - mean(xs * ys)) / ((mean(xs)**2) - mean(xs*xs)))
-    return m
+    b = mean(ys) - m * mean(xs)
+    return m, b
 
-m = best_fit_slope(xs, ys)
+m, b = best_fit_slope_and_intercept(xs, ys)
 
-print(m)
+print(m, b)
+
+
