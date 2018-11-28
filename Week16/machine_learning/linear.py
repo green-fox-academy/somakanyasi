611908d3c2a4b152f4fc77543c28f526f93725f1
somakanyasi <kanyasi.soma@gmail.com>
2018-11-29 00:43:01 +0100

    best fit slope calculation

--- Week16/machine_learning/linear.py
+++ Week16/machine_learning/linear.py
@@ -2,8 +2,13 @@ from statistics import mean
 import numpy as np
 import matplotlib.pyplot as plt
 
-xs = [1,2,3,4,5,6]
-ys = [5,4,6,5,6,7]
+xs = np.array([1,2,3,4,5,6], dtype=np.float64)
+ys = np.array([5,4,6,5,6,7], dtype=np.float64)
 
-plt.plot(xs,ys)
-plt.show()
+def best_fit_slope(xs, ys):
+    m = (((mean(xs) * mean(ys)) - mean(xs * ys)) / ((mean(xs)**2) - mean(xs*xs)))
+    return m
+
+m = best_fit_slope(xs, ys)
+
+print(m)
