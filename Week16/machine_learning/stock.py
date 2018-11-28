703b15123883f3d363be85a559775cd07b72ef13
somakanyasi <kanyasi.soma@gmail.com>
2018-11-29 00:01:52 +0100

    matplotlib imported

--- Week16/machine_learning/stock.py
+++ Week16/machine_learning/stock.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import quandl
-import math
+import math, datetime
 import numpy as np
 from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
 
 df = quandl.get('WIKI/GOOGL')
 df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
@@ -20,11 +24,15 @@ forecast_out = int(math.ceil(0.01*len(df)))
 print(forecast_out)
 
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace=True)
 
 X = np.array(df.drop(['label'], 1))
-y = np.array(df['label'])
 X = preprocessing.scale(X)
+X = X[:-forecast_out]
+# X_lately is what we're going to predict
+X_lately = X[-forecast_out:]
+
+df.dropna(inplace=True)
+y = np.array(df['label'])
 y = np.array(df['label'])
 
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
@@ -33,6 +41,9 @@ clf = LinearRegression()
 clf.fit(X_train, y_train)
 # tells us how accurately we can predict into the future(forecast_out)
 accuracy = clf.score(X_test, y_test)
-
 print(accuracy)
+
+forecast_set = clf.predict(X_lately)
+
+print(forecast_set, forecast_out)
 
